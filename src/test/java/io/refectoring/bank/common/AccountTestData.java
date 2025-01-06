f207388c867eb06de72f560cc0d746d4ee2d8c7d
hmmini <pak4184@naver.com>
2025-01-07 00:10:00 +0900

    도메인 엔티티 단위테스트하기 - 테스트 데이터

---
+++ src/test/java/io/refectoring/bank/common/AccountTestData.java
@@ -0,0 +1,49 @@
+package io.refectoring.bank.common;
+
+import io.refectoring.bank.account.domain.Account;
+import io.refectoring.bank.account.domain.ActivityWindow;
+import io.refectoring.bank.account.domain.Money;
+
+import static io.refectoring.bank.account.domain.Account.*;
+
+public class AccountTestData {
+
+    public static AccountBuilder defaultAccount() {
+        return new AccountBuilder()
+                .withAccountId(new AccountId(42L))
+                .withBaselineBalance(Money.of(999L))
+                .withActivityWindow(new ActivityWindow(
+                        ActivityTestData.defaultActivity().build(),
+                        ActivityTestData.defaultActivity().build()));
+    }
+
+
+    public static class AccountBuilder {
+
+        private AccountId accountId;
+        private Money baselineBalance;
+        private ActivityWindow activityWindow;
+
+        public AccountBuilder withAccountId(AccountId accountId) {
+            this.accountId = accountId;
+            return this;
+        }
+
+        public AccountBuilder withBaselineBalance(Money baselineBalance) {
+            this.baselineBalance = baselineBalance;
+            return this;
+        }
+
+        public AccountBuilder withActivityWindow(ActivityWindow activityWindow) {
+            this.activityWindow = activityWindow;
+            return this;
+        }
+
+        public Account build() {
+            return withId(this.accountId, this.baselineBalance, this.activityWindow);
+        }
+
+    }
+
+
+}
