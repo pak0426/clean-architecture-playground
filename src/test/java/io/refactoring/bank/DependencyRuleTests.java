c39b2c3332555a15e1f2d170514c35d9cfd5b4cb
hmmini <pak4184@naver.com>
2025-01-18 17:06:02 +0900

    헥사고날 아키텍처 패키지 의존성 테스트

--- src/test/java/io/refactoring/bank/DependencyRuleTests.java
+++ src/test/java/io/refactoring/bank/DependencyRuleTests.java
@@ -4,6 +4,7 @@ import com.tngtech.archunit.core.domain.JavaClasses;
 import com.tngtech.archunit.core.importer.ClassFileImporter;
 import com.tngtech.archunit.junit.AnalyzeClasses;
 import com.tngtech.archunit.lang.ArchRule;
+import io.refactoring.bank.archunit.HexagonalArchitecture;
 import org.junit.jupiter.api.Test;
 
 import static com.tngtech.archunit.lang.syntax.ArchRuleDefinition.*;
@@ -21,5 +22,23 @@ public class DependencyRuleTests {
                 .resideOutsideOfPackages("io.refactoring.bank.account.application.."); // application 패키지 외의 다른 패키지에만
 
         rule.check(classes);
+    }
+
+    @Test
+    void validate_registration_context_architecture() {
+        HexagonalArchitecture.boundedContext("io.refactoring.bank.account")  // 전체 패키지 경로 지정
+                .withDomainLayer("domain")
+                    .withAdaptersLayer("adapter")
+                    .incoming("web")
+                    .outgoing("persistence")
+                    .and()
+                .withApplicationLayer("application")
+                    .services("service")
+                    .incomingPorts("port.in")
+                    .outgoingPorts("port.out")
+                    .and()
+                .withConfiguration("configuration")
+                .check(new ClassFileImporter()
+                        .importPackages("io.refactoring.bank..")); // 전체 패키지를 스캔
     }
 }
